88e46890819bc78814c13fdd5528c7b5d47af39e
Adam <weinels@gmail.com>
2019-09-21 13:05:09 -0500

    Scrolling now goes to top of first page.

--- init.el
+++ init.el
@@ -44,7 +44,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(helm-mode t)
- '(package-selected-packages (quote (rainbow-mode))))
+ '(package-selected-packages (quote (rainbow-mode)))
+ '(scroll-error-top-bottom t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
