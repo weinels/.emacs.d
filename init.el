44e7d6566d6a7c0effc399c6731947b088c17006
Adam <weinels@gmail.com>
2019-09-21 13:39:48 -0500

    Changed syntax highlighting colors.

--- init.el
+++ init.el
@@ -51,6 +51,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-builtin-face ((t (:foreground "color-27"))))
  '(font-lock-comment-face ((t (:foreground "brightyellow" :weight bold))))
  '(font-lock-constant-face ((t (:foreground "color-123"))))
  '(font-lock-function-name-face ((t (:foreground "color-208"))))
