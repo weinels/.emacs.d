d22771a3074ee55bdfa423921d5dd64b35b3daca
Adam <weinels@gmail.com>
2019-09-21 16:45:12 -0500

    ecb refresh methods on save.

--- init.el
+++ init.el
@@ -69,6 +69,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ecb-auto-update-methods-after-save t)
  '(ecb-options-version "2.50")
  '(helm-mode t)
  '(package-selected-packages (quote (rainbow-mode)))
